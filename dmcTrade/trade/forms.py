09390776027de9fd35f0b85d3c14e378aecc70a5
Peter Collingridge <peter.collingridge@gmail.com>
2019-05-04 22:48:33 +0100

    Create order form

---
+++ dmcTrade/trade/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class OrderForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    sender = forms.EmailField()
+    order = forms.CharField(widget=forms.Textarea)
+
