52500c4182b369103db44f3a9b2ed45aabca1838
Peter Collingridge <peter.collingridge@gmail.com>
2019-05-05 22:26:55 +0100

    Change sender to email in form

--- dmcTrade/trade/forms.py
+++ dmcTrade/trade/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class OrderForm(forms.Form):
     name = forms.CharField(max_length=100)
-    sender = forms.EmailField()
+    email = forms.EmailField()
     order = forms.CharField(widget=forms.HiddenInput(), required=False)
     
