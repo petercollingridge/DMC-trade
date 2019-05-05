5372b6e07647962ce12b5d812b8f5c79df080343
Peter Collingridge <peter.collingridge@gmail.com>
2019-05-05 22:27:23 +0100

    Add vue to deal with dynamic order form

--- dmcTrade/static/js/dmcTrade.js
+++ dmcTrade/static/js/dmcTrade.js
@@ -1,18 +1,17 @@
-var orderList = document.getElementById('order-list');
-var orders = {};
+var vm = new Vue({
+    el: '#order-list',
+    data: {
+        order: {},
+        total: 0
+    },
+});
 
 function updateOrder(event) {
+    var item = event.target.getAttribute('data-name');
     var value = event.target.value;
-    var name = event.target.getAttribute('data-name');
-    
-    if (!orders[name]) {
-        // Add element
-        var newItem = document.createElement('div');
-        var newContent = document.createTextNode(name); 
-
-        newItem.appendChild(newContent); 
-        orderList.appendChild(newItem);
-    }
+    var changeInTotal = value - (vm.order[item] || 0);
+    Vue.set(vm.order, item, value);
+    vm.total += changeInTotal;
 }
 
 // Add event handlers to drops down elements;
