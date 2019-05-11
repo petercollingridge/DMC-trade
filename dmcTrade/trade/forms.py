9becea39491011186ca0801bc9d8030eb598d2ac
Peter Collingridge <peter.collingridge@gmail.com>
2019-05-11 14:49:41 +0100

    Add more fields for form

--- dmcTrade/trade/forms.py
+++ dmcTrade/trade/forms.py
@@ -2,7 +2,12 @@ from django import forms
 
 
 class OrderForm(forms.Form):
+    required_css_class = 'required'
+    
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
+    billing_address = forms.CharField()
+    delivery_address = forms.CharField()
+    comments = forms.CharField(required=False)
     order = forms.CharField(widget=forms.HiddenInput(), required=False)
     
